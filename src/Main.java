b54a5b4e4c6fbf08ed6fcfc956ee0f19b823dfae
Aditya-785 <adiish2367@gmail.com>
2024-11-18 11:44:59 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        /* 1st program for our coding  journey !!!*/
         System.out.println("Hello World");
+
     }
 }
---
+++ src/hello_world.java
@@ -0,0 +1,9 @@
+public class hello_world {
+    public static void main(String[] args) {
+        // Here is our program to print hello world in console//
+        // this is the starting journey of any programmeer //
+        System.out.println("Hello World");
+
+        // Now we return to 0 //
+    }
+}
