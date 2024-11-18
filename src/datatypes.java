b203ba3df007aed02ecdb44867f4c0c604a457f9
Aditya-785 <adiish2367@gmail.com>
2024-11-18 15:53:30 +0530

    First Commit

---
+++ src/datatypes.java
@@ -0,0 +1,28 @@
+public class datatypes {
+    public static void main(String[] args) {
+
+        //Now first we store some data into variables .
+        // In Java, data types are used to declare the type of data that a variable can hold.
+        int a = 12;
+        int b = 15;
+        // Here are two variable which store some data in integers , now we proceed with some arithmetic problems -
+        int sum = a+b;  // sum of  two variaels
+        int diff = a-b;  // diffrence of two variables
+        int pro = a*b; // multiplication of two variables
+        int div = b/a; // divison of two vzriables
+
+        System.out.println("sum :" + sum);
+        System.out.println("diff :" + diff);
+        System.out.println("product :"+ pro);
+        System.out.println("divison :" +div);
+
+        /*now we have to
+        print some values of variables which is tore in integers datatype
+        which is gemrerally came in Primitive datatype -
+
+        Then return to 0
+         */
+
+
+    }
+}
